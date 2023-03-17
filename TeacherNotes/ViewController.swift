ef651f31c470c390083002130c8d21388e7bd449
emininmac <mamihaha4@gmail.com>
2023-03-17 14:56:30 +0300

    Initial Commit

---
+++ TeacherNotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TeacherNotes
+//
+//  Created by Muhammed Emin Bardakcı on 17.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
